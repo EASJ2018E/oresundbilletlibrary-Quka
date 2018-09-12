161a4cb2af58132293a167dece796fee0b1acb39
Arlind Ukshini <quka93@gmail.com>
2018-09-12 18:40:19 +0200

    methods done + 100% code coverage

--- OresundBilletLibrary/Bil.cs
+++ OresundBilletLibrary/Bil.cs
@@ -4,6 +4,23 @@ namespace OresundBilletLibrary
 {
 	public class Bil : BilletLibrary.Bil
 	{
+		/// <summary>
+		/// Returns the price of admission for Øresundsbrosen for Car
+		/// 420 is standard price, 161 is price with brobizz
+		/// </summary>
+		/// <returns>int 420 or 161 if brobizz is used</returns>
+		public override decimal Pris()
+		{
+			return Brobizz ? 161 : 420;
+		}
 
+		/// <summary>
+		/// Returns the type (køretøj)  of this vehicle 
+		/// </summary>
+		/// <returns>Returns the vehicle type (køretøj)</returns>
+		public override string Type()
+		{
+			return "Øresund Bil";
+		}
 	}
 }
--- OresundBilletLibrary/MC.cs
+++ OresundBilletLibrary/MC.cs
@@ -4,8 +4,25 @@ using System.Text;
 
 namespace OresundBilletLibrary
 {
-	class MC : BilletLibrary.MC
+	public class MC : BilletLibrary.MC
 	{
+		/// <summary>
+		/// Returns the price of admission for Øresundsbrosen for MC
+		/// 210 is standard price, 73 is price with brobizz
+		/// </summary>
+		/// <returns>int 210 or 73 if brobizz is used</returns>
+		public override decimal Pris()
+		{
+			return Brobizz ? 73 : 210;
+		}
 
+		/// <summary>
+		/// Returns the type (køretøj)  of this vehicle
+		/// </summary>
+		/// <returns>Returns the vehicle type (køretøj)</returns>
+		public override string Type()
+		{
+			return "Øresund MC";
+		}
 	}
 }
